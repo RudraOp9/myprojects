a1a56e0824b535bbe797bd07201fbef2ac9f477d
Leo <100891455+RudraOp9@users.noreply.github.com>
2023-06-29 17:15:24 +0530

    Create Kilometer to miles convertor.java

---
+++ Kilometer to miles convertor.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class MyClass {
+  public static void main(String args[]) {
+    //Kilometer to miles convertor
+    // 1 km = 0.621 miles
+    float km = 0.621f;
+    Scanner sc= new Scanner(System.in);
+    System.out.println("enter kilometer");
+    float a = sc.nextInt();
+    float miles = km*a;
+    System.out.println(a + " kilometers is equals to " + miles + " miles");
+    
+    
+  }
+}
