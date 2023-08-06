c37c1204f288febd529f2adc1db401720b569ef7
Leo <100891455+RudraOp9@users.noreply.github.com>
2023-08-06 18:57:08 +0530

    Add files via upload

---
+++ ignore/practise.java
@@ -0,0 +1,34 @@
+
+public class practise {
+
+    int num(int a, int b) {
+
+        int x = 0;
+        int y = 0;
+        int z = 0;
+       while (a<20 && b<20) {
+           x=a;
+           y=b;
+           z= x+y;
+          // int cum = num(x, y);
+           System.out.print(z + " ");
+           a=b;
+           b=z;
+
+
+       }
+        return x;
+    }
+    public static void main(String[] args) {
+        // formula Fn = Fn-1+Fn-2
+        int a1 = 1;
+        int a2 = 1;
+        System.out.print(a1 + " " + a2 + " ");
+        practise obj = new practise() ;
+     int sib = obj.num(1,1);
+
+       // System.out.println( x );
+        //fibbonacci series 1 1 2 3 5 8 13
+
+    }
+}
